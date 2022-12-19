f7f01130573e5a7dfbe169b28ad6b4370a97fdd5
Imaad <101230925+imaadf@users.noreply.github.com>
2022-12-19 04:14:30 -0700

    Added python3 solution to Two Sum

---
+++ python3-solutions/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        compliment_to_index = {}
+
+        for index, num in enumerate(nums):
+            compliment = target - num
+            if(compliment in compliment_to_index):
+                return [index, compliment_to_index[compliment]]
+            compliment_to_index[num] = index
+
+        return None
